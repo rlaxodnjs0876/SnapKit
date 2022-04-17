73b27554833e70f78d74d080c4dad9441c8522c7
Kim <xodnjs0876@gmail.com>
2022-04-17 23:16:28 +0900

    [ADD] SnapKit Pod

---
+++ SnapKitTest/Pods/Target Support Files/Pods-SnapKitTest-SnapKitTestUITests/Pods-SnapKitTest-SnapKitTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapKitTest_SnapKitTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapKitTest_SnapKitTestUITests
+@end
--- SnapKitTest/SnapKitTest/ViewController.swift
+++ SnapKitTest/SnapKitTest/ViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+        
+    var redView = UIView()
+    var orangeView = UIView()
+    var yellowView = UIView()
+    var greenView = UIView()
+    var blueView = UIView()
+    var indigoView = UIView()
+    var purpleView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(redView)
+        self.view.addSubview(orangeView)
+        self.view.addSubview(yellowView)
+        self.view.addSubview(greenView)
+        self.view.addSubview(blueView)
+        self.view.addSubview(indigoView)
+        self.view.addSubview(purpleView)
+        
+        redView.backgroundColor = .red
+        orangeView.backgroundColor = .orange
+        yellowView.backgroundColor = .yellow
+        greenView.backgroundColor = .green
+        blueView.backgroundColor = .blue
+        indigoView.backgroundColor = .systemIndigo
+        purpleView.backgroundColor = .purple
+        
+        redView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.size.width.height.equalTo(100)
+            make.left.equalTo(view.snp.left)
+        }
+        
     }
 
 
